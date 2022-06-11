7c3b2319442d0053cecfa6ae366a8bb38ce4318f
Daniel Trenado <datregurgmail.com>
2022-06-11 06:33:29 +0200

    updated local repo

--- CC 10-06-22/Exes and Ohs.js
+++ CC 10-06-22/Exes and Ohs.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 
 Examples input/output:
@@ -18,4 +19,26 @@ function XO(str) {
   }else{
     return false
   }
+=======
+/*Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false*/
+
+
+function XO(str) { 
+    //str to uppercase and then to arr
+    let arr = str.toLowerCase().split('');
+    //filter out the 'x' and 'o' in new arrays and compare their length; same length is true, else false
+    if (arr.filter( letter => letter === 'x').length ===  arr.filter( letter => letter === 'o').length) { 
+    return true
+  }else{
+    return false
+  }
+>>>>>>> addf0cbfbe692428e77b443f06a9f81b897d46c7
 }
--- CC 10-06-22/Grasshopper - Check for factor.js
+++ CC 10-06-22/Grasshopper - Check for factor.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*DESCRIPTION:
 This function should test if the factor is a factor of base.
 
@@ -16,4 +17,24 @@ Note: base is a non-negative number, factor is a positive number.*/
 
 // if remainder of base / factor equals 0 is true, else false
 
+=======
+/*DESCRIPTION:
+This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+
+About factors
+Factors are numbers you can multiply together to get another number.
+
+2 and 3 are factors of 6 because: 2 * 3 = 6
+
+You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+You can use the mod operator (%) in most languages to check for a remainder
+For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+Note: base is a non-negative number, factor is a positive number.*/
+
+// if remainder of base / factor equals 0 is true, else false
+
+>>>>>>> addf0cbfbe692428e77b443f06a9f81b897d46c7
 checkForFactor = (base, factor) => base % factor === 0 ? true : false
