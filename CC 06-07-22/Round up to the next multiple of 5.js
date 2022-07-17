f0d1221c108a2a2d1cfb0f8a183ba17c35c468f4
Daniel TG <datregu@gmail.com>
2022-07-17 17:24:08 +0200

    fixing errors

--- CC 06-07-22/Round up to the next multiple of 5.js
+++ CC 06-07-22/Round up to the next multiple of 5.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
 
 Examples:
@@ -24,4 +25,32 @@ roundToNext5 = n => {
         n +=1
       }
     }
+=======
+/*Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers.*/
+
+roundToNext5 = n => {
+    for ( let i = 0; i < 5; i++){
+      if ( n %  5 === 0){
+        return n
+      }else{
+        n +=1
+      }
+    }
+>>>>>>> bf02c823e483479775ce01c92f680f013f6dd526
     }
--- CC 08-07-22/Array-diff.js
+++ CC 08-07-22/Array-diff.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
 It should remove all values from list a, which are present in list b keeping their order.
@@ -7,4 +8,15 @@ If a value is present in b, all of its occurrences must be removed from the othe
 
 arrayDiff([1,2,2,2,3],[2]) == [1,3]*/
 //Filter all values from array 'a' included in array 'b'
+=======
+/*Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]*/
+//Filter all values from array 'a' included in array 'b'
+>>>>>>> bf02c823e483479775ce01c92f680f013f6dd526
 arrayDiff = (a, b) => a.filter(val=>!b.includes(val))
