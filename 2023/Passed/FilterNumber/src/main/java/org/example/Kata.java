123ccb2398326155c3e4c17d553e70c2859360fa
Daniel TG <datregu@gmail.com>
2023-01-24 10:09:38 +0100

    new kata of the day

---
+++ 2023/Passed/FilterNumber/src/main/java/org/example/Kata.java
@@ -0,0 +1,18 @@
+/*
+Filter the number
+
+        Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+        Task
+
+        Your task is to return a number from a string.
+        Details
+
+        You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+*/
+
+package org.example;
+public class Kata {
+    public static long filterString(final String value) {
+        return Long.parseLong(value.replaceAll("[^0-9]", ""));
+    }
+}
---
+++ 2023/Passed/FilterNumber/src/test/java/org/example/KataTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KataTest {
+    @Test
+    public void pruebas() {
+        assertEquals(123,"123");
+        //assertEquals("a123",123);
+       // assertEquals("asdsa5645",5645);
+    }
+}
